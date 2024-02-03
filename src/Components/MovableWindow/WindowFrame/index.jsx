ba8154338944532acbdc8ef2056f1b692a514fa5
XCPika <61687817+XCPika@users.noreply.github.com>
2024-02-03 15:02:35 +0000

    Started grid based layout system for absolute elements
    Positioning function seems to be working as intented. Needs some work to try and make the values input to it nicer to work with and make the function trigger nicely on window resize (may have complications for window position after they are moved, windows would snap back to original position on resize) CHECK COMMENTS IN "WindowFrame/index.jsx"

--- src/Components/MovableWindow/WindowFrame/index.jsx
+++ src/Components/MovableWindow/WindowFrame/index.jsx
@@ -4,37 +4,39 @@ import { useContext } from "react";
 import { PositionContext } from "../WindowManager/context/PositionContext";
 
 const WindowFrame = (props) => {
+    // POSITION CONTEXT => Gives access to mouse coordinates, updates on mouse movement
     const mousePos = useContext(PositionContext);
     const [bDragging, setDragging] = useState(false);
     const [bMinimized, setMinimized] = useState(false);
     const [prevPos, setPrevPos] = useState({ x: props.pos.x, y: props.pos.y});
     const [pos, setPos] = useState({ x: props.pos.x, y: props.pos.y});
+
     var element = document.getElementById(props.title);
 
     const handleMouseDown = (e) => {
         e.preventDefault();
+        // NEEDS WORK: Proposed change, use another context provider to increment an index constantly starting from 10 to give room for other elements and ending at 2,000,000 before looping. 
+        // prevents int overflow and saves updating an array/object with window zindexs
+        // Context/Provider should originate in WindowManager and might require it to pass a function into children and return the data. 
+        // That way when a window is clicked it can run the provided function, incrementing the index and then setting it on the target
+        // ISSUES: the current proposed idea doesnt handle for what to do after a window has been moved and may require some extra logic in WindowManager that makes storing an object worth while
         element.parentElement.parentElement.style.zIndex = 99;
-        console.log("Pointer Down")
-        setPrevPos({ x: mousePos.x, y: mousePos.y})
-        setDragging(true)
+        setPrevPos({ x: mousePos.x, y: mousePos.y});
+        setDragging(true);
     }
 
     const handleMouseUp = (e) => {
         e.preventDefault();
-        console.log("Pointer Up")
+        // SEE COMMENT IN handleMouseDown
         element.parentElement.parentElement.style.zIndex = "auto";
-        setDragging(false)
+        setDragging(false);
     }
 
     const handleMouseMove = () => {
-        console.log("Pos:", pos)
-        console.log("prev:", prevPos, mousePos)
-        console.log("Difference: ", {x:  (prevPos.x - mousePos.x), y: (prevPos.y - mousePos.y)})
-        console.log("offset:", element.offsetTop, element.offsetLeft)
-        setPos(
-            { x: (pos.x - (prevPos.x - mousePos.x)), 
-              y: (pos.y - (prevPos.y - mousePos.y)) }
-        )
+        setPos({ 
+            x: (pos.x - (prevPos.x - mousePos.x)), 
+            y: (pos.y - (prevPos.y - mousePos.y)) 
+        })
         setPrevPos({ x: mousePos.x, y: mousePos.y})
     }
     
@@ -53,7 +55,7 @@ const WindowFrame = (props) => {
         className={`window $bMinimized ? 'minimized' : ''}`}
         style={{ position: "absolute", top: `${pos.y}px`, left: `${pos.x}px`}}
     >
-        <div className="window-border" style={{height: "400px", width: "400px"}}>
+        <div className="window-border" style={{height: "150px", width: "150px"}}>
             <div className="window-header" 
                 id={props.title}
                 onMouseDown={handleMouseDown} 
--- src/Pages/Team/index.jsx
+++ src/Pages/Team/index.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import { WindowFrame } from '../../Components'
 
+const normalize = (val, max, min) => { return (val - min) / (max - min) }
+
 export default function Team() {
+
+    const getPosition = (x, y, row, column, gap) => {
+        // done: ratio of window size to 1000x800 (1.2)
+        // done: apply ratio to x, y and gap to ensure scaling
+        // todo: FUNCTION STILL NEEDS A GOOD WAY TO RERUN ON RESCALE
+        var winSize = {x: normalize(window.innerWidth, 1000, 0), y: normalize(window.innerHeight, 800, 0)}
+        console.log(winSize)
+        x = x * winSize.x
+        y = y * winSize.y
+        return {
+            x: x + (column * (gap * winSize.x)),
+            y: y + (row * (gap * winSize.y))
+        }
+    }
+
     return (
         <div>
-            <WindowFrame title="Random Title" pos={{x: (window.innerWidth / 6) * 1, y: 200}}>
+            <WindowFrame title="Random Title" pos={getPosition(25, 250, 0, 0, 250)}>
                 <p style={{margin: 0}}>Welcome to IR Freelance</p>
             </WindowFrame>
-            <WindowFrame title="Test" pos={{x: window.innerWidth/6 * 3, y: 200}}>
+            <WindowFrame title="Test" pos={getPosition(25, 250, 0, 1, 250)}>
                 <p style={{margin: 0}}>Welcome to IR Freelance</p>
             </WindowFrame>
-            <WindowFrame title="Showcase" pos={{x: window.innerWidth/6 * 3, y: 200}}>
+            <WindowFrame title="Showcase" pos={getPosition(25, 250, 0, 2, 250)}>
+                <p style={{margin: 0}}>Welcome to IR Freelance</p>
+            </WindowFrame>
+            <WindowFrame title="Showcase" pos={getPosition(25, 250, 0, 3, 250)}>
                 <p style={{margin: 0}}>Welcome to IR Freelance</p>
             </WindowFrame>
         </div>
