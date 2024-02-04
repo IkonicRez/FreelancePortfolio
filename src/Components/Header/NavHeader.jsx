90d0c42a293a5e111243db8d819bf0d76ea62067
XCPika <61687817+XCPika@users.noreply.github.com>
2024-02-04 16:37:06 +0000

    Fixed error in links

--- src/Components/Header/NavHeader.jsx
+++ src/Components/Header/NavHeader.jsx
@@ -5,12 +5,12 @@ const NavHeader = () => {
   return (
     <div>
       <header id='App-header'>
-        <Link href="/">
+        <Link to="/">
           <h1>IRFreelance</h1>
         </Link>
         <nav id='header-nav'>
-            <Link href="/team">Team</Link>
-            <Link href="/showcase">Showcase</Link>
+            <Link to="/team">Team</Link>
+            <Link to="/showcase">Showcase</Link>
         </nav>
       </header>
     </div>
